b1e7db99b108d3fa70c8dd5fa371713cf755d65c
s30391 <s30391@pjwstk.edu.pl>
2025-03-09 18:28:52 +0100

    METODA

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿class Program
+{
+    public static double ObliczSrednia(int[] liczby)
+    {
+        int suma = 0;
+        foreach (int liczba in liczby)
+        {
+            suma += liczba;
+        }
+        return (double)suma / liczby.Length;
+    }
+
+    static void Main()
+    {
+        int[] liczby = { 1, 2, 3, 4, 5 };
+            double srednia = ObliczSrednia(liczby);
+            Console.WriteLine("Średnia: " + srednia);
+        
+    }
+}
